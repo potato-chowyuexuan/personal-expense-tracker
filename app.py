086d3e925a9a033915867cc2cb6c34690d325414
Chow Yue Xuan <chowyuexuan@gmail.com>
2026-01-22 12:33:06 +0800

    Added /expenses/<int:expense_id>

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__) #create a Flask application instance
 
+
+
 #Simple route for testing
 @app.route('/') #at this route(URL path), the function home will be executed
 def home():
@@ -8,6 +10,7 @@ def home():
 
 if __name__ == '__main__': #runs if this script is run directly (not imported as a module)
     app.run(debug=True) #run the application in debug mode(for learning and development purposes)
+
 
 
 #POST endpoint to create a new expense
@@ -48,6 +51,8 @@ def create_expense():
         }
     }), 201 #HTTP status code for created
 
+
+
 #GET endpoint to retrieve all expenses
 @app.route('/expenses', methods=['GET'])
 def get_expenses():
@@ -62,6 +67,25 @@ def get_expenses():
     ] #list comprehension to create a list of expense dictionaries
     return jsonify(expenses_list), 200
 
+
+
+#GET endpoint to retrieve a single expense by ID
+@app.route("/expenses/<int:expense_id>", methods=['GET'])
+def get_expense(expense_id):
+    expense = Expense.query.get(expense_id)
+    if not expense: 
+        return jsonify({"error": f"Expense with id {expense_id} not found"}), 404
+    
+    expense_data = {
+        "id": expense.id,
+        "title": expense.title,
+        "amount": expense.amount,
+        "date": expense.date
+    }
+    return jsonify(expense_data), 200
+
+
+
 #DELETE endpoint to delete an expense by ID
 @app.route("/expenses/<int:expense_id>", methods=['DELETE']) #methods act like an instruction to Flask on how to handle requests 
 def delete_expense(expense_id):
@@ -73,3 +97,34 @@ def delete_expense(expense_id):
     db.session.commit() #commit the changes
 
     return jsonify({"message": f"Expense with id {expense_id} deleted successfully"}), 200
+
+
+
+#PUT endpoint to update an existing expense by ID
+@app.route("/expenses/<int:expense_id>", methods = ["PUT"])
+def update_expense(expense_id):
+    expense = Expense.query.get(expense_id) #fetch the expense record with the given ID from the database
+    if not expense:
+        return jsonify({"error": "No input data provided"}), 400
+
+    data = request.get_json() #takes raw HTTP body and converts it into a Python dictionary, if not JSON, returns None
+    if not data: #check if JSON data is provided
+        return jsonify({"error": "No input data provided"}), 400
+    if "title" in data:
+        expense.title = data["title"]
+    if "amount" in data:
+        expense.amount = data["amount"]
+    if "date" in data:
+        expense.date = data["date"]
+
+    db.session.commit()
+
+    return jsonify({
+        "message": f"Expense with id {expense_id} updated successfully",
+        "expense": {
+            "id": expense.id,
+            "title": expense.title,
+            "amount": expense.amount,
+            "date": expense.date
+        }
+    }), 200
